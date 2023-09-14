39fb456f92b2ec6fb66a5f1ff3e16dafe02af0cc
Irine7 <irine7@yandex.ru>
2023-09-14 16:45:51 +0200

    feat: add header & footer components

---
+++ organick/src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Logo from '../utils/Logo';
+import Socials from '../utils/Socials';
+
+function Footer() {
+	const infoTitles = ['Email', 'Phone', 'Address'];
+	const infoText = [
+		'info@organick.com',
+		'123 456-789',
+		'123 Main St, Anytown, CA 12345',
+	];
+	const utilityPages = [
+		'Licenses',
+		'Password Protected',
+		'Changelog',
+		'404 Not Found',
+];
+
+	return (
+		<div>
+			<div className="footer">
+				<div className="footer__container">
+					<div className="footer__contacts">
+						<h5>Contact Us</h5>
+						<div className="footer__contacts-info">
+							{infoTitles.map((title, index) => (
+								<>
+									<p key={index} className="footer__contacts-info__title">{title}</p>
+									<p className="footer__contacts-info__text">{infoText[index]}</p>
+								</>
+							))}
+						</div>
+					</div>
+					<div className="footer__socials">
+						<Logo />
+						<p className="footer__socials-text">
+							Simply dummy text of the printing and typesetting industry. Lorem
+							Ipsum simply dummy text of the printing
+						</p>
+						<div className="footer__socials-icons">
+							<Socials />
+						</div>
+					</div>
+					<div className="footer__pages">
+						<h5>Utility Pages</h5>
+						{utilityPages.map((page, index) => (
+							<p key={index}>{page}</p>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
--- organick/src/components/Header.jsx
+++ organick/src/components/Header.jsx
@@ -1,29 +1,22 @@
+import Logo from '../utils/Logo';
+import Input from '../utils/InputContainer';
+import Cart from '../utils/Cart';
+
 function Header() {
 	const menu = ['Home', 'About', 'Pages', 'Shop', 'Projects', 'News'];
 
 	return (
 		<div className="header">
-			<div className="container">
-				<a href="/">
-					<div className="header__logo">
-						<img width="36" src="assets/img/logo.svg" alt="Organick logo" />
-						<h4>Organick</h4>
-					</div>
-				</a>
+			<div className="header__container">
+				<Logo />
 				<nav className="header__menu">
 					{menu.map((item, index) => (
 						<span key={index}> {item}</span>
 					))}
 				</nav>
 				<div className="header__cart">
-					<div class="input-container">
-						<input type="text" />
-						<img src="assets/img/header/search.svg" alt="" class="icon" />
-					</div>
-					<div className="header__cart-block">
-						<img src="assets/img/header/cart.svg" alt="" />
-						<span>Cart(0)</span>
-					</div>
+					<Input />
+					<Cart />
 				</div>
 			</div>
 		</div>
